59349486cbab8d79fbca83c06dd73ec94e65b5d9
Mohammad Javad Tabari <mohammadJavadTabari1024@outlook.com>
2023-04-02 05:29:29 -0700

    rs1dev1

---
+++ develop/RSI/rsi_dev1.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+from talib import RSI
+import MetaTrader5 as mt5
+from datetime import datetime
+
+lot = 0.02
+
+# Get the Data
+
+mt5.initialize()
+
+while True:
+    symbol_info=mt5.symbol_info("EURUSD")
+    rates = mt5.copy_rates_from_pos("EURUSD", mt5.TIMEFRAME_M1, 0, 1000)
+    rates_frame = pd.DataFrame(rates)
+
+    rates_frame["time"]=pd.to_datetime(rates_frame["time"], unit="s")
+
+    #Create signal
+
+    close = np.array(rates_frame["close"])
+
+    #Create the RSI indicator
+
+    rates_frame["rsi"] = RSI(close, timeperiod = 14)
+
+    #Create the Rolling mean
+
+    rates_frame["rsi_roll_mean"] = rates_frame["rsi"].rolling(10).mean()
+
+    #Create the Rolling std
+
+    rates_frame["rsi_roll_std"] = rates_frame["rsi"].rolling(10).std()
+
+    #Create the custom signal
+
+    rates_frame["custom signal"] = np.where(rates_frame["rsi"] > 2*rates_frame["rsi_roll_std"], "Sell",
+
+    np.where(rates_frame["rsi"] < 2*rates_frame["rsi_roll_std"], "Buy", ""))
+
+    print('time: ', datetime.now())
+    # print('exposure: ', exposure)
+    # print('last_close: ', last_close)
+    # print('sma: ', sma)
+    # print('signal: ', direction)
+    # print(SYMBOL)
+    print("position is opened. ")
+    print('-------\n')
+
